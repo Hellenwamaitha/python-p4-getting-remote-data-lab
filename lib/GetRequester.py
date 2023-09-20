8ae43145fb71a7662c584df886e4713355dd2eda
hellenwamaitha <hellenwama254@gmail.com>
2023-09-20 16:23:53 +0300

    updated lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,41 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            response.raise_for_status()  # Raise an exception for HTTP errors (e.g., 404)
+            return response.text  # Return the response body as text
+        except requests.exceptions.RequestException as e:
+            print(f"Error: {e}")
+            return None  # Return None in case of an error
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()  # Fetch the response body
+        if response_body is not None:
+            try:
+                data = json.loads(response_body)  # Parse the response body as JSON
+                return data
+            except json.JSONDecodeError as e:
+                print(f"JSON Decode Error: {e}")
+                return None  # Return None in case of JSON decoding error
+
+# Example usage:
+if __name__ == "__main__":
+    # Initialize the GetRequester with a URL
+    url = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+    requester = GetRequester(url)
+
+    # Get and print the response body
+    response_body = requester.get_response_body()
+    print("Response Body:")
+    print(response_body)
+
+    # Load JSON data from the response body and print it
+    json_data = requester.load_json()
+    if json_data:
+        print("\nJSON Data:")
+        print(json_data)
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -4,11 +4,14 @@ class GetRequesterTest:
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
 JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
 CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
-
 def test_get_response():
-        '''get_response_body function returns response.'''
-        requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+    '''get_response_body function returns response.'''
+    requester = GetRequester(URL)
+    response_body = requester.get_response_body()
+
+    response_string = response_body
+    assert response_string == JSON_STRING.decode('utf-8')
+
 
 def test_load_json():
         '''load_json function returns response.'''
